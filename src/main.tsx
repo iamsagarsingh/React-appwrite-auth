533c092c61004aee23ba81a3423f4233e8d08de9
sagar singh <sagar.17jccs049@jietjodhpur.ac.in>
2024-07-08 16:34:48 +0530

    implement protected routing.

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,51 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.tsx";
+import "./index.css";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { Home } from "./pages/Home.tsx";
+import { Login } from "./pages/Login.tsx";
+import { Signup } from "./pages/Signup.tsx";
+import { AuthLayout } from "./pages/AuthLayout.tsx";
+import { UserContextProvider } from "./context/UserContext.tsx";
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    children: [
+      {
+        path: "/",
+        element: (
+          <AuthLayout authentication={false}>
+            <Home />
+          </AuthLayout>
+        ),
+      },
+      {
+        path: "/login",
+        element: (
+          <AuthLayout authentication={true}>
+            <Login />
+          </AuthLayout>
+        ),
+      },
+      {
+        path: "/signup",
+        element: (
+          <AuthLayout authentication={true}>
+            <Signup />
+          </AuthLayout>
+        ),
+      },
+    ],
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <UserContextProvider>
+      <RouterProvider router={router} />
+    </UserContextProvider>
+  </React.StrictMode>
+);
