da3458e65bc949cae885661ff5736fde94337c75
sagar singh <sagar.17jccs049@jietjodhpur.ac.in>
2024-07-10 01:11:43 +0530

    Add password recovery and email verifcation methods.

--- src/appwrite/Auth.ts
+++ src/appwrite/Auth.ts
@@ -21,19 +21,21 @@ class AuthService {
         name
       );
       if (userAcc) {
-        return this.Login(email, password);
+        this.Login(email,password).then(()=>{
+          this.createUserVerification()
+        })
       } else {
         return userAcc;
       }
-    } catch {
-      console.log("Signup error.");
+    } catch(err:any) {
+      if(err.code === 409) alert("Email id already registered.")
     }
   }
   async Login(email: string, password: string) {
     try {
       return await this.account.createEmailPasswordSession(email, password);
-    } catch {
-      console.log("Login Error.");
+    } catch(err:any) {
+      if(err.code === 401) alert("Invalid Email id or password!!")
     }
   }
 
@@ -47,11 +49,55 @@ class AuthService {
 
   async Logout() {
     try {
-        await this.account.deleteSessions()
+      await this.account.deleteSessions();
     } catch {
       console.log("Logout Error.");
     }
   }
+
+  async recoverPassword(email: string) {
+    try {
+      await this.account.createRecovery(
+        email,
+        "http://localhost:5173/reset-password"
+      );
+    } catch {
+      console.log("Password Recover Err.");
+    }
+  }
+
+  async updatePasswordRecovery(userId:string,secret:string,password:string){
+    try{
+      await this.account.updateRecovery(userId,secret,password)
+    }
+    catch{
+      console.log("Update Recovery Password");
+      
+    }
+  }
+
+  async createUserVerification(){
+    try{
+        await this.account.createVerification('http://localhost:5173/verfiy-user')
+        alert("check email and click on the verification link.")
+    }
+    catch(err){
+      console.log("email verification fail.",err);
+      
+    }
+  }
+
+  async updateUserVerification(userId:string,secret:string){
+    try{
+        return await this.account.updateVerification(userId,secret)
+        
+    }
+    catch{
+      console.log("fail || update userVerfication.");
+      
+    }
+  }
+
 }
 
 export const authService = new AuthService();
